582bef80f69f86df2cc918b91f2d51706ede915b
mapellil <licio.mapelli@st.com>
2020-04-07 15:38:31 +0200

    Added demo startup args

--- test/demo_commsdk.py
+++ test/demo_commsdk.py
@@ -1,4 +1,4 @@
-import sys
+import sys, argparse
 import os
 
 from abc import ABCMeta
@@ -47,123 +47,134 @@ class M4_sdb_rx_listener (RpmsgSdbAPIListener):
 
 #========================================================
 # MAIN APPLICATION
-
 #
 # Main application.
 #
 def main(argv):     
 
     try:
+
+        parser = argparse.ArgumentParser(description='Run a demo with associated M4 Fw.')
+        parser.add_argument('demo', type=str, help='The demo to be run: <commsdk> or <sdbsdk> ')
+
+        parser.add_argument('m4fw', type=str, help='The associated m4 fw to be run: <OpenAMP_TTY_echo.elf> or  <how2eldb03110.elf>')
+
+        args = parser.parse_args()
+        print ('Input test is ', args.test)
+        print ('M4 fw file is ', args.m4fw)
+
         print ("Entering main Py")
+        if (args.test == "sdbsdk"):
 
-        ser_obj = CommAPI("/dev/ttyRPMSG0", None, "how2eldb03110.elf")            
-        sdb_obj = RpmsgSdbAPI(None)  
+#            ser_obj = CommAPI("/dev/ttyRPMSG0", None, "how2eldb03110.elf")            
+            ser_obj = CommAPI("/dev/ttyRPMSG0", None, args.m4fw)            
+            sdb_obj = RpmsgSdbAPI(None)  
 
-        m4_sdb_listener = M4_sdb_rx_listener()
-        sdb_obj.add_sdb_buffer_rx_listener(m4_sdb_listener)                   
+            m4_sdb_listener = M4_sdb_rx_listener()
+            sdb_obj.add_sdb_buffer_rx_listener(m4_sdb_listener)                   
 
-        answ = ser_obj.cmd_set("r", 0)    # send run cmd to M4 FW, (wait blocking for answ)       
-        print ("--->M4: ","r")
-        while (answ.find("boot successful") == -1):
-            answ = ser_obj.cmd_get(None, 0)        
-            print ("<---M4: ", answ)   
-
-        sdb_obj.init_sdb(1024*1024, 3)  # shared data buffers bufsize, bufnum
-
-        answ = ser_obj.cmd_set("S002M", 0)  # send sampling freq cmd to M4 FW (wait blocking for answ)       
-        while (len(answ) > 0):
-            answ = ser_obj.cmd_get(None, 0)        
-            print ("<---M4: ", answ)   
-         
-        sdb_obj.start_sdb_receiver();  # start the user side drv receiver thread, the on_M4_sdb_rx will be triggered by M4 when sdb is ready
-
-     
-        i=0
-        while (i < 20):     # let sys run for 10S serial getting msgs from M4 if any
-            time.sleep(0.5)                   
-            i=i+1
-            while (len(answ) > 0):
+            answ = ser_obj.cmd_set("r", 0)    # send run cmd to M4 FW, (wait blocking for answ)       
+            print ("--->M4: ","r")
+            while (answ.find("boot successful") == -1):
                 answ = ser_obj.cmd_get(None, 0)        
                 print ("<---M4: ", answ)   
 
-        
-        print("Stopping everything .....")
-        answ = ser_obj.cmd_set("Exit", 0)    # send stop cmd to M4 FW, (wait blocking for answ) 
-        while (len(answ) > 0):
-            answ = ser_obj.cmd_get(None, 0)        
-            print ("<---M4: ", answ)   
-                      
-        sdb_obj.stop_sdb_receiver();  # stop the sdb receiver thread (make it ready to start again)
-        sdb_obj.deinit_sdb()    # exit the rx th, close open files and signals, unmap the sdb memory, stop the M4 FW         
+            sdb_obj.init_sdb(1024*1024, 3)  # shared data buffers bufsize, bufnum
 
-        raise SystemExit      
-        return
+            answ = ser_obj.cmd_set("S002M", 0)  # send sampling freq cmd to M4 FW (wait blocking for answ)       
+            while (len(answ) > 0):
+                answ = ser_obj.cmd_get(None, 0)        
+                print ("<---M4: ", answ)   
+             
+            sdb_obj.start_sdb_receiver();  # start the user side drv receiver thread, the on_M4_sdb_rx will be triggered by M4 when sdb is ready
+
+         
+            i=0
+            while (i < 30):     # let sys run for 15S and serial getting msgs from M4 if any
+                time.sleep(0.5)                   
+                i=i+1
+                while (len(answ) > 0):
+                    answ = ser_obj.cmd_get(None, 0)        
+                    print ("<---M4: ", answ)   
+
+            
+            print("Stopping everything .....")
+            answ = ser_obj.cmd_set("Exit", 0)    # send stop cmd to M4 FW, (wait blocking for answ) 
+            while (len(answ) > 0):
+                answ = ser_obj.cmd_get(None, 0)        
+                print ("<---M4: ", answ)   
+                          
+            sdb_obj.stop_sdb_receiver();  # stop the sdb receiver thread (make it ready to start again)
+            sdb_obj.deinit_sdb()    # exit the rx th, close open files and signals, unmap the sdb memory, stop the M4 FW         
+
+            raise SystemExit      
+            return
                  
 
 #**********************************************   End sdb test   *****************************************************
+        elif (args.test == "commsdk"):
         
-        api_obj = CommAPI("/dev/ttyRPMSG0", \
-                          "/dev/ttyRPMSG1", \
-                          "OpenAMP_TTY_echo.elf" )   
-        m4_answ_listener = M4_answ_listener() 
-        m4_ntfy_listener = M4_ntfy_listener()
+            api_obj = CommAPI("/dev/ttyRPMSG0", \
+                              "/dev/ttyRPMSG1", \
+                              "OpenAMP_TTY_echo.elf" )   
+            m4_answ_listener = M4_answ_listener() 
+            m4_ntfy_listener = M4_ntfy_listener()
 
 
-#        evt_answ.clear()
-#        api_obj.add_answers_listener(m4_answ_listener)
+    #        evt_answ.clear()
+    #        api_obj.add_answers_listener(m4_answ_listener)
 
-#        with open('lena.jpg', rb) as file
-#            finished = False
-#            while (not finished)
-#                datard = file.read (10000)
-#                if datard != 10000
-#                    finished =True
-#                if api_obj.cmd_set(datard, 2) == -1:
-#                    print ("API Locked: retry!")
-#                evt_answ.wait()
+    #        with open('lena.jpg', rb) as file
+    #            finished = False
+    #            while (not finished)
+    #                datard = file.read (10000)
+    #                if datard != 10000
+    #                    finished =True
+    #                if api_obj.cmd_set(datard, 2) == -1:
+    #                    print ("API Locked: retry!")
+    #                evt_answ.wait()
 
-
-        while True:
-
-            print ("Blocking cmd_get ...")                
-            datard = api_obj.cmd_get("Prova blk", 0)
-            print ("Returned: ",datard)
-
-            datawr = 123
-            datard = api_obj.cmd_set(datawr, 0)
-            print ("Returned: ",datard)
 
             while True:
-                pass
 
-            evt_answ.clear()
-            api_obj.add_answers_listener(m4_answ_listener)
+                print ("Blocking cmd_get ...")                
+                datard = api_obj.cmd_get("Prova blk", 0)
+                print ("Returned: ",datard)
 
-            print ("Non blocking cmd_get 1 ...")                        
-            if api_obj.cmd_get("Prova non blk 1", 2) == -1:
-                print ("API Locked: retry!")
-            evt_answ.wait()
+                datawr = 123
+                datard = api_obj.cmd_set(datawr, 0)
+                print ("Returned: ",datard)
 
-	# TODO ack the M4 FW to not answer this cmd allowing response timeout to expire
-            evt_answ.clear()
-            print ("Non blocking cmd_get 2 ...(with no answ from M4 so timed out)")   
-            if api_obj.cmd_get("Prova non blk 2", 2) == -1:
-                print ("API Locked: retry!")        
-            evt_answ.wait() 
+                while True:
+                    pass
 
-	# async notify test
-            evt_ntfy.clear()
-            api_obj.add_notifications_listener(m4_ntfy_listener)
-            print ("Enter in a separate shell: echo Prova ntfy > /dev/ttyRPMSG1 ")
-            evt_ntfy.wait()
+                evt_answ.clear()
+                api_obj.add_answers_listener(m4_answ_listener)
 
-            print ("Exiting test")
-            api_obj.remove_notifications_listener(m4_ntfy_listener)
-            api_obj.remove_answers_listener(m4_answ_listener)
+                print ("Non blocking cmd_get 1 ...")                        
+                if api_obj.cmd_get("Prova non blk 1", 2) == -1:
+                    print ("API Locked: retry!")
+                evt_answ.wait()
 
-# TODO stop M4 FW
-        sys.exit(0)      
-        os._exit(0)
+    	# TODO ack the M4 FW to not answer this cmd allowing response timeout to expire
+                evt_answ.clear()
+                print ("Non blocking cmd_get 2 ...(with no answ from M4 so timed out)")   
+                if api_obj.cmd_get("Prova non blk 2", 2) == -1:
+                    print ("API Locked: retry!")        
+                evt_answ.wait() 
+
+    	# async notify test
+                evt_ntfy.clear()
+                api_obj.add_notifications_listener(m4_ntfy_listener)
+                print ("Enter in a separate shell: echo Prova ntfy > /dev/ttyRPMSG1 ")
+                evt_ntfy.wait()
+
+                print ("Exiting test")
+                api_obj.remove_notifications_listener(m4_ntfy_listener)
+                api_obj.remove_answers_listener(m4_answ_listener)
+
+            sys.exit(0)      
+            os._exit(0)
 
     except KeyboardInterrupt:
         try:	
@@ -175,7 +186,7 @@ def main(argv):
 
 #    except BTLEDisconnectError as e:
     except () as e:
-        logging.debug("\n====>>>EXCEPTION!!!! Ble Node Disconnected", + str(e))                
+        logging.debug("\n====>>>EXCEPTION! ", + str(e))                
 
 
 if __name__ == "__main__":
